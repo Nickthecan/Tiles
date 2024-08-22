6312ed64078b4f300e8dbf175b5e68167fa4bff5
Nicholas Amancio <112116021+Nickthecan@users.noreply.github.com>
2024-08-22 16:14:22 -0700

    added random colors to the tiles

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const colors = ["#c04343", "#43cc77", "#439acc", "#ccca43"]
+/* const colors = ["#c04343", "#43cc77", "#439acc", "#ccca43"] */
+const colors = ["green", "blue", "yellow", "red"];
 
 function createSquare() {
     var area = document.getElementById("tileSpace");
 
     console.log("making squares");
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 5000; i++) {
         const newTile = document.createElement("div");
         newTile.className = "squareTile";
-        const randomColor = colors[Math.floor(Math.random) * colors.length];
-        newTile.style.backgroundColor = randomColor;
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        newTile.classList.add(randomColor);
         area.appendChild(newTile);
     }
 }
